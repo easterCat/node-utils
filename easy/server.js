0e0e04849f67b4d0db3506882054c5765576a349
easterCat <fuhuodemao@163.com>
2019-03-20 11:10:43 +0800

    commonjs快速提交=>2019-3-20 11:10:43

--- easy/server.js
+++ easy/server.js
@@ -1,17 +1,17 @@
 /**
  * Created by easterCat on 2017/8/22.
  */
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 
 function start(route, handle) {
-    function onRequest(request, response) {
-        let pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received.");
-        route(handle, pathname, response, request);
-    }
+  function onRequest(request, response) {
+    let pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
+  }
 
-    http.createServer(onRequest).listen(4444);
-    console.log('server has started 4444');
+  http.createServer(onRequest).listen(4444);
+  console.log("server has started 4444");
 }
 exports.start = start;
